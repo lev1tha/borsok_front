788a8a35d65c3ee0296dd8914891716ccec43645
Eldos <up1mepdev@gmail.com>
2024-08-29 11:56:49 +0600

    create-login-page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import style from "./app.module.css";
-
+// component && logic for router
 import Header from "../widget/header";
 import Footer from "../widget/footer";
 import { Route, Routes } from "react-router-dom";
+// pages component
 import Home from "../pages/home";
 import Cart from "../pages/cart";
 import Admin from "../pages/admin";
+import Login from "../pages/login";
 
 const App = () => {
   const url = window.location.href;
 
-  console.log(url);
-
   return (
     <div className={style.container}>
-      {url.includes("/admin") || <Header />}
+      {url.includes("/admin") || url.includes("/login") || <Header />}
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/cart"} element={<Cart />} />
         <Route path={"/admin"} element={<Admin />} />
+        <Route path={"/login"} element={<Login />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Logo from "../../assets/svg/borsok-logo.svg";
+import style from "./login.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faPhone, faUser } from "@fortawesome/free-solid-svg-icons";
+import api from "../../shared/lib/api";
+
+// TODO Надо будет удалить поле password а так дальше реализовать отправку запроса на сервер
+
+export default function Index() {
+  const [phoneNumber, setPhoneNumber] = useState("+996 ");
+  const [buttonActive, setButtonActive] = useState(false);
+  const [visibility, setVisibility] = useState(false);
+
+  const handlePhoneNumberChange = (e) => {
+    const input = e.target.value;
+
+    if (!input.startsWith("+996")) {
+      setPhoneNumber("+996 ");
+    } else {
+      const formattedInput = "+996 " + input.slice(5).replace(/\D/g, "");
+      setPhoneNumber(formattedInput);
+    }
+
+    if (input) setButtonActive(true);
+    else setButtonActive(false);
+  };
+
+  const handleShowPassword = () => {
+    setVisibility((prev) => !prev);
+  };
+
+  // const handleSendRegistration = () => {
+  //   api.post("register/").then((res) => {
+  //     res.data;
+  //   });
+  // };
+
+  return (
+    <div className={style.container}>
+      <div className={style.login_container}>
+        <img src={Logo} alt="logotype" />
+        <form action="" className={style.container_form}>
+          <div className={style.box_form}>
+            <FontAwesomeIcon icon={faUser} />
+            <input type="text" placeholder="Ваше имя" />
+          </div>
+          <div className={style.box_form}>
+            <FontAwesomeIcon icon={faPhone} />
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+              placeholder="+996 Ваш номер телефона"
+              className={style.phoneInput}
+            />
+          </div>
+          <div className={style.box_form}>
+            <FontAwesomeIcon icon={faEye} onClick={handleShowPassword} />
+            <input
+              type={visibility ? "text" : "password"}
+              placeholder="Пароль"
+            />
+          </div>
+        </form>
+        <button
+          // onClick={handleSendRegistration()}
+          className={buttonActive ? style.buttonVisible : ""}
+        >
+          Зарегистрироваться
+        </button>
+      </div>
+    </div>
+  );
+}
